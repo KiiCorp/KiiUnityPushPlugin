f36ec38ac80f8931449eae782d5542e9362fd28a
Riza <riza.alaudin@kii.com>
2014-10-23 20:41:36 +0900

    Implement push notification with dependency injection.

---
+++ IOSPushPlugin/CustomPushClass/CustomPush.h
@@ -0,0 +1,13 @@
+//
+//  CustomPush.h
+//  Unity-iPhone
+//
+//  Created by Syah Riza on 10/23/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "PushRegister.h"
+@interface CustomPush : NSObject<PushRegister>
+
+@end
